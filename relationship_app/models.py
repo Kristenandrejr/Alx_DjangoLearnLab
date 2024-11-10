b7d9ad99397a5a532421fa016c986614291d7810
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-10 10:17:34 +0100

    Update models.py

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,3 +1,5 @@
+# relationship_app/models.py
+
 from django.db import models
 
 class Author(models.Model):
@@ -6,14 +8,12 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -22,10 +22,10 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
-
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
