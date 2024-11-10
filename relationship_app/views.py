d4ae20089f5c7fa5d1d30a501d482b4b2d7f679f
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-10 14:18:51 +0100

    Update views.py

--- relationship_app/views.py
+++ relationship_app/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Book
 
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # Get all books
+    book_list = [{"title": book.title, "author": book.author} for book in books]  # Extract titles and authors
+    return JsonResponse({"books": book_list})
 
+from django.http import JsonResponse
 from django.views.generic import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'  # The template to display the library details
-    context_object_name = 'library'  # The context name used in the template
+    context_object_name = 'library'
+
+    def render_to_response(self, context, **kwargs):
+        library = context['library']
+        books = [{"title": book.title} for book in library.books.all()]  # List books in the library
+        return JsonResponse({"library": library.name, "books": books})
