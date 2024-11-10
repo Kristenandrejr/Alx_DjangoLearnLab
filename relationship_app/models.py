a271b3d12e2a41289567cfcf06ddd70501effb2d
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-10 12:59:18 +0100

    Update models.py

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -4,11 +4,20 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     publication_date = models.DateField()
 
+    def __str__(self):
+        return self.title
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} at {self.library_name}"
