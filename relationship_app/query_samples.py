8412afb1d60ac932008c230e58154e62ba3223b7
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-10 12:56:27 +0100

    Update query_samples.py

--- relationship_app/query_samples.py
+++ relationship_app/query_samples.py
@@ -1,44 +1,17 @@
-<<<<<<< HEAD
-# relationship_app/query_samples.py
-
-from relationship_app.models import Author, Book, Library, Librarian
-
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)
-    for book in books:
-        print(book.title)
-
-def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()
-    for book in books:
-        print(book.title)
-
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library)
-    print(librarian.name)
-=======
-from relationship_app.models import Author, Book, Library, Librarian
-
-# Query all books by a specific author
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)
-    for book in books:
-        print(book.title)
+from relationship_app.models import Book, Author, Librarian
 
 # List all books in a library
-def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()
-    for book in books:
-        print(book.title)
+def list_books_in_library(library_name):
+    books = Book.objects.filter(library_name=library_name)
+    return books
+
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
 
 # Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library)
-    print(librarian.name)
->>>>>>> b7d9ad99397a5a532421fa016c986614291d7810
+def librarian_for_library(library_name):
+    librarian = Librarian.objects.filter(library_name=library_name).first()
+    return librarian
