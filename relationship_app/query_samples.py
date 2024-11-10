c4c319fdfbed80e4cf04b772b465af587c0855b9
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-10 10:12:37 +0100

    Update query_samples.py

--- relationship_app/query_samples.py
+++ relationship_app/query_samples.py
@@ -1,16 +1,21 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-def books_by_author(author_name):
+def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+    books = Book.objects.filter(author=author)
+    for book in books:
+        print(book.title)
 
 # List all books in a library
-def books_in_library(library_name):
+def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    books = library.books.all()
+    for book in books:
+        print(book.title)
 
 # Retrieve the librarian for a library
-def librarian_of_library(library_name):
+def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return Librarian.objects.get(library=library)
+    librarian = Librarian.objects.get(library=library)
+    print(librarian.name)
