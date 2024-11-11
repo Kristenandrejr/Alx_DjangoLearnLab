a4ad7cbf444ff503222197368237b7b2c94c334a
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-11 11:09:13 +0100

    Update models.py

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -11,15 +11,9 @@ class Book(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-<<<<<<< HEAD
     library_name = models.CharField(max_length=100)
 
     def __str__(self):
-<<<<<<< HEAD
         return f"{self.name} at {self.library_name}"
-=======
         return self.name
->>>>>>> d5fd5adc1b87251729b840f3145415bb0e655970
-=======
     library = models.CharField(max_length=100)
->>>>>>> c02058f04dfc8336a35a6571e2e3f95e094b3df3
