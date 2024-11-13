b0720fa4b2bb5f7653a8779beefedee4d10feb76
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-13 14:05:24 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,20 +3,19 @@ from . import views  # Import your views
 from django.contrib.auth.views import LoginView, LogoutView  # Import LoginView and LogoutView
 
 urlpatterns = [
+    # Books and Library Details
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    
+
     # Custom Authentication Views
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     
-    # Built-in Authentication Views with template paths (for Savanna checks)
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login_builtin'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout_builtin'),
+    # Built-in Authentication Views (For Savanna check)
+    path('login_builtin/', LoginView.as_view(template_name='relationship_app/login.html'), name='login_builtin'),
+    path('logout_builtin/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout_builtin'),
     
-    # User registration URL
+    # Registration and Home views
     path('register/', views.register, name='register'),
-    
-    # Home view after login
     path('home/', views.home, name='home'),
 ]
