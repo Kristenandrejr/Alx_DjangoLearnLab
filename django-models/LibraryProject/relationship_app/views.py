d2fe2819181d65c39f0e42e4c954acccf0655bf5
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-14 13:38:04 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,24 +1,30 @@
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
-# View to add a book
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book(request):
-    # Your code for adding a book goes here
-    return render(request, 'add_book.html')
+# Check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-# View to edit a book
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    # Your code for editing a book goes here
-    return render(request, 'edit_book.html', {'book': book})
+# Check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
-# View to delete a book
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    # Your code for deleting a book goes here
-    return render(request, 'delete_book.html', {'book': book})
+# Check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
