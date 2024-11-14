6e04e0c56ce6b30558ea5b0f6c2a2228c64e142b
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-14 12:24:58 +0100

    Update models.py

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -36,9 +36,9 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f"{self.user.username} - {self.role}"
-
+        return self.user.username
