7e0f113a9ceccb95fc00c7ff9226674aaf0a3ede
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-14 12:56:23 +0100

    Update models.py

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -41,4 +41,4 @@ class UserProfile(models.Model):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username} - {self.role}"
