06e07fd4f2c315260de9dde58a54701ad33df03b
Kristenandrejr <kristenkrisjr@gmail.com>
2024-11-14 13:01:29 +0100

    Update models.py

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,35 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Existing models
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-# New UserProfile model for Role-based Access Control
 class UserProfile(models.Model):
     ROLE_CHOICES = (
         ('Admin', 'Admin'),
