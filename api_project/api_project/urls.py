a81d37f43fe3ed33815418066363218243ec6ab2
Christian Edeh <kristenkrisjr@gmail.com>
2024-11-28 22:07:51 +0100

    Complete task updates for library project

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for advanced_api_project project.
+URL configuration for api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
