6dfbf88566c69e35153127190ab201b105f1f8a1
Kristenandrejr <kristenkrisjr@gmail.com>
2024-12-02 11:59:22 +0100

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-BASE_DIR / "blog/static",
 STATICFILES_DIRS = [
     BASE_DIR / "static", 
 ]
