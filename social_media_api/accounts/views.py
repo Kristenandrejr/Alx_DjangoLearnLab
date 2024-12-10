92b0de78fb83f2df1bb016fe07658ae809eaf98b
Kristenandrejr <kristenkrisjr@gmail.com>
2024-12-10 15:12:56 +0100

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,26 +1,36 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import CustomUser
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-@api_view(['POST'])
-def follow_user(request, user_id):
-    try:
-        user_to_follow = CustomUser.objects.get(id=user_id)
-        if request.user != user_to_follow:
-            request.user.following.add(user_to_follow)
-            return Response({'message': f'You are now following {user_to_follow.username}'}, status=200)
-        return Response({'error': 'You cannot follow yourself.'}, status=400)
-    except CustomUser.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=404)
+# Existing ViewSets
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    try:
-        user_to_unfollow = CustomUser.objects.get(id=user_id)
-        if request.user != user_to_unfollow:
-            request.user.following.remove(user_to_unfollow)
-            return Response({'message': f'You have unfollowed {user_to_unfollow.username}'}, status=200)
-        return Response({'error': 'You cannot unfollow yourself.'}, status=400)
-    except CustomUser.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=404)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+# Added user_feed Function
+@api_view(['GET'])
+def user_feed(request):
+    """
+    Returns a feed of posts created by the users that the requesting user follows.
+    """
+    followed_users = request.user.following.all()  # Assuming a 'following' field exists on the user model
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
