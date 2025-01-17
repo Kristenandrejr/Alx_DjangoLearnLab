e7c704ea8243af48f99d8ec56b55fb95d6b14cbe
Kristenandrejr <kristenkrisjr@gmail.com>
2025-01-17 20:08:15 +0100

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.contrib.auth.models import Group, Permission
+from .models import CustomUser, Book
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -21,4 +22,26 @@ class CustomUserAdmin(UserAdmin):
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
+# Register the Book model
+admin.site.register(Book)
 
+# Create and assign permissions to groups in the Django admin interface
+def create_groups_and_permissions():
+    group_editors, created = Group.objects.get_or_create(name='Editors')
+    group_viewers, created = Group.objects.get_or_create(name='Viewers')
+    group_admins, created = Group.objects.get_or_create(name='Admins')
+
+    # Permissions for Editors
+    can_edit = Permission.objects.get(codename='can_edit')
+    can_create = Permission.objects.get(codename='can_create')
+    group_editors.permissions.set([can_edit, can_create])
+
+    # Permissions for Viewers
+    can_view = Permission.objects.get(codename='can_view')
+    group_viewers.permissions.set([can_view])
+
+    # Permissions for Admins
+    can_delete = Permission.objects.get(codename='can_delete')
+    group_admins.permissions.set([can_view, can_create, can_edit, can_delete])
+
+create_groups_and_permissions()
